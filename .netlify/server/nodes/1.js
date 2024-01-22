39bde23f2c41b7b561bd8bdb6171f6a0186357be
guillaumelaine <null>
2024-01-22 18:34:07 +0100

    add headers to netlify.toml

--- .netlify/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.HLszDj9f.js","_app/immutable/chunks/scheduler.65lUaMOu.js","_app/immutable/chunks/index.WGX1G8Nk.js","_app/immutable/chunks/entry.gZ3l8jqY.js","_app/immutable/chunks/index._ppBPcxw.js"];
-export const stylesheets = [];
-export const fonts = [];
