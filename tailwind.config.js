e35e58fde89f34c9fa389ea47b39a636b63c26b7
guillaumelaine <null>
2024-02-04 14:51:36 +0100

    quick fix for tailwinf in prod, modify config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
   theme: {
     extend: {}
   },
